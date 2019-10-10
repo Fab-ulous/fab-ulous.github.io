cfb1a9d885f09aa8fa931b5accf9151e1a34df72
Matteo Manzinello <matteo@manzinello.com>
2019-10-10 22:39:15 +0200

    gatsby

---
+++ README.md
@@ -0,0 +1,5 @@
+# ⭕️ fab-ulous
+
+![](./src/images/github-fabulous.png)
+
+<https://fab-ulous.github.io>
---
+++ src/components/_ui/Logo.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+import logo from "../../images/fab-ulous.png"
+
+const Logo = () => <img src={logo} width="200" />
+
+export default Logo
